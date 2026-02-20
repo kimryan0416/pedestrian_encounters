6727707a1241d4e06bcfbed6b7eab3c07e7b2cda
Ryan Kim <ryankim@Ryans-MacBook-Pro.local>
2026-02-20 15:36:42 -0500

    attempting to make subprocess listen to the current virtual environment via 'sys.executable'

--- pages/1_Recording_EEG.py
+++ pages/1_Recording_EEG.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import subprocess
+import sys
 
 st.set_page_config(page_title="Recording EEG", page_icon="📈")
 
@@ -20,7 +21,10 @@ with col1:
     st.header("Demo")
     st.markdown("You can invoke this operation to just see if your BlueMuse system is working. It produces a set of dynamic graphs that visualize the various data channels recorded by BlueMuse. This is **purely** for visualization and debugging; it doesn't actually record.")
     if st.button("Run Demo Visualization"):
-        subprocess.Popen([".venv/bin/python", "RecordMuse/record/demo.py"])
+        subprocess.Popen([
+            sys.executable, 
+            "RecordMuse/record/demo.py"
+        ])
 
 with col2:
     st.header("Recording")
